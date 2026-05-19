5e58fa6b992171be632812eb2bcd81740fbba279
Praba151 <prabakaranbaktha@gmail.com>
2026-05-19 15:40:10 +0530

    base url change

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,9 @@
-import axios from 'axios';
-const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
+import axios from "axios";
+const API = axios.create({
+  baseURL: "https://counselling-backend-app.onrender.com/api",
+});
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
